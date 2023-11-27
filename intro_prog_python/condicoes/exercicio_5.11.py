1607162fdbbb4b302834e254f3061715de47a087
Fernando Oliveira <fernandooliveira@live.com>
2023-11-27 10:57:44 +0000

    exercicio 5.11

---
+++ intro_prog_python/condicoes/exercicio_5.11.py
@@ -0,0 +1,9 @@
+def main():
+    # Entrada
+    deposito  = int(input("Digite o valor do depósito inicial: "))
+    taxa_juros = int(input("Digite o valor da taxa de juros: "))
+
+    # Processamento
+    
+if __name__ == '__main__':
+    main()
