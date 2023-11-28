b207d0ffa9c429860be3773d4d6d38a8bfa080ea
Fernando Oliveira <fernandooliveira@live.com>
2023-11-28 20:52:45 -0300

    exercicio 3.7 e 3.8

---
+++ intro_prog_python/caput_3_variaveis/exercicio_3.7.py
@@ -0,0 +1,10 @@
+def main():
+
+    num1 = int(input("Insira um número inteiro: "))
+    num2 = int(input("Insira outro número inteiro: "))
+
+    print(f"A soma dos dois números digitados é: {num1+num2}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ intro_prog_python/caput_3_variaveis/exercicio_3.8.py
@@ -0,0 +1,7 @@
+def main():
+
+    medida = int(input("Informe a medida em metros: "))
+    print(f"{medida} metros convertidos em milímetros resularam em: {medida * 1000} milímetros")
+
+if __name__ == '__main__':
+    main()
