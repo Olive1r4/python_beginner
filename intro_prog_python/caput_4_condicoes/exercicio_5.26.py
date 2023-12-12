8c65e6a824411ad42629b3006c8fe46612f7e061
Fernando Oliveira <fernandooliveira@live.com>
2023-12-12 11:00:33 +0000

    Exercicios 5.26

--- intro_prog_python/caput_4_condicoes/exercicio_5.26.py
+++ intro_prog_python/caput_4_condicoes/exercicio_5.26.py
@@ -4,12 +4,14 @@ def main():
     num2 = int(input("Informe o segundo numero inteiro: "))
 
     num3 = 0
+    soma = 0
 
     while True:
-        if num2 < num1:
-            num2 += num2
+        if (num2 + num2) < num1:
+            soma += num2
         else:
-            num3 = num1 - num2
-
+            num3 = num1 - soma
+            print(f"Restor: {num3}")
+            break
 if __name__=='__main__':
     main()
