b6a35335526b0d73de8147275b51e45eade3d10d
Fernando Oliveira <fernandooliveira@live.com>
2023-12-12 10:44:47 +0000

    Exercicios 5.26

--- intro_prog_python/caput_4_condicoes/exercicio_5.26.py
+++ intro_prog_python/caput_4_condicoes/exercicio_5.26.py
@@ -1,16 +1,15 @@
 def main():
 
+    num1 = int(input("Informe o primeiro numero inteiro: "))
+    num2 = int(input("Informe o segundo numero inteiro: "))
+
+    num3 = 0
+
     while True:
-        num1 = int(input("Informe o primeiro numero inteiro: "))
-        num2 = int(input("Informe o segundo numero inteiro: "))
-        if num1 != int:
-            print("Você digitou um númro inválido")
-            num1 = int(input("Informe o primeiro numero inteiro: "))
-        elif num2 != int:
-            print("Você digitou um númro inválido")
-            num2 = int(input("Informe o primeiro numero inteiro: "))
+        if num2 < num1:
+            num2 += num2
         else:
-            break
-                   
+            num3 = num1 - num2
+
 if __name__=='__main__':
     main()
