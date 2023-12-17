8a8da4df61cb0545fb4d6dc3b25924bbf3a4ab40
Fernando Oliveira <fernandooliveira@live.com>
2023-12-17 10:20:20 -0300

    exercicio 6_1

--- intro_prog_python/caput_6/exercicio_6_1.py
+++ intro_prog_python/caput_6/exercicio_6_1.py
@@ -1,16 +1,16 @@
 # Programa 6.1 - Calculo de média
 def main():
 
-    notas = [0, 0, 0, 0, 0]
+    notas = [0, 0, 0, 0, 0, 0, 0]
     soma = 0
     x = 0
-    while x < 5:
-        notas[x] = float(input(f"Notas {x}: "))
+    while x < 7:
+        notas[x] = float(input(f"Notas {x+1}: "))
         soma += notas[x]
         x += 1
     x = 0
-    while x < 5:
-        print(f"Nota {x}: {notas[x]:6.2f}")
+    while x < 7:
+        print(f"Nota {x+1}: {notas[x]:6.2f}")
         x += 1
     print(f"Média: {soma / x:5.2f}")
 if __name__=='__main__':
